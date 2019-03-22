40222b32aaba7af0289a3d1c1a7a094a7b077f6d
Amita <amita.parikh@gmail.com>
2019-03-22 22:42:16 +0000

    downloadimagebyURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets.js');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -12,15 +13,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': secrets.GITHUB_TOKEN
     }
   };
+//download image by URL
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+    .pipe(fs.createWriteStream(filePath));
+  }
+  console.log(downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg"));
 
+//request parse data
   request(options, function(err, res, body) {
     // console.log('result:', JSON.parse(body));
     cb(err, JSON.parse(body));
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+
+getRepoContributors("ap2006", "tweeter", function(err, result) {
   // console.log("Errors:", err);
+  console.log(result);
   result.forEach(function(element) {
     console.log('USER---------', element.avatar_url);
   })
